5e60f276d98f26f9fd4609a842e0a21637af6fea
ryanmills-dev <ryanmillsdev@outlook.com>
2026-03-23 14:07:32 +0000

    Added: - Controller for Projects

---
+++ Api/Controllers/ProjectsController.cs
@@ -0,0 +1,25 @@
+﻿using Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly DevForgeDbContext _context;
+
+        public ProjectsController(DevForgeDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+        {
+            return await _context.Projects.ToListAsync(); 
+        }
+    }
+}
